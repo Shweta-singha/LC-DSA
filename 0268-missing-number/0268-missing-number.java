ba311effb64f9b6fe5fa80b9c7d4b18495d69130
Shweta Singha <iamshweta.singha@gmail.com>
2026-06-20 02:06:56 +0530

    Time: 0 ms (100%), Space: 47.1 MB (81.05%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        int n = nums.length;
+
+        int[] freq = new int[n + 1];
+
+        for(int i = 0; i < nums.length; i++) {
+            freq[nums[i]]++;
+        }
+
+        for(int i = 0; i < freq.length; i++) {
+            if(freq[i] == 0) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
