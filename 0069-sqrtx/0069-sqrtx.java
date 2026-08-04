18ef2b20a47141d719076c13455c5568ea3b92f6
Shweta Singha <iamshweta.singha@gmail.com>
2026-08-04 23:08:32 +0530

    Time: 1 ms (97.59%), Space: 42.8 MB (8.47%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int mySqrt(int x) 
+    {
+        int low = 0; 
+        int high = x;
+
+        int answer = 0; 
+
+        while(low<=high)
+        {
+            int mid = low + (high - low)/2;
+            long square = (long) mid * mid;
+
+            if(square == x)
+            {
+                return mid;
+            }
+
+            else if(square<x)
+            {
+                answer = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return answer;
+    }
+}
