024837491b3e21e263579976027820f29b3bf87e
Shweta Singha <iamshweta.singha@gmail.com>
2026-08-10 11:11:48 +0530

    Time: 0 ms (100%), Space: 44.5 MB (14.13%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,22 +9,20 @@
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head) {
+    public ListNode reverseList(ListNode head) 
+    {
+        ListNode prev = null; 
+        ListNode current = head; 
 
-        ListNode prev = null;
-        ListNode curr = head;
+        while(current != null){
+            ListNode next = current.next; 
+            current.next = prev; 
+            prev = current;
+            current = next;
 
-        while(curr != null) {
-
-            ListNode next = curr.next;
-
-            curr.next = prev;
-
-            prev = curr;
-
-            curr = next;
         }
-
         return prev;
+
+        
     }
 }
